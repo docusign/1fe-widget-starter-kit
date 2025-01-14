3da92107da0c4a11de9d617ca12d962e8f1b1de3
avanish.pathak <avanish.pathak@docusign.com>
2025-01-14 14:00:19 -0800

    feat(turbo): use monorepo the right way

---
+++ src/app1.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Root() {
+  return (
+    <p>My component from app1 is mounted! Hello world</p>
+  );
+}
---
+++ src/root.component.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default function Root() {
+  return (
+    <section>My amazing component from app1 is mounted! Hello world</section>
+  );
+}
