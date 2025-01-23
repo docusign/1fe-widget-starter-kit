5c2f941767328ef13646cd016356ff1f7f8dc00b
abe.hu <abe.hu@docusign.com>
2025-01-22 16:53:18 -0800

    library polling and preloads

--- src/app1.tsx
+++ src/app1.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function Root() {
-  return (
-    <p>My component from app1 is mounted! Hello world</p>
-  );
+  return <p>My component from app1 is mounted! Hello world</p>;
 }
