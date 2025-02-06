999d81776a136b4fde67c96dac01c93fb4dc1f33
abe.hu <abe.hu@docusign.com>
2025-02-05 18:10:36 -0800

    i hate store2

--- src/app1.tsx
+++ src/app1.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Root() {
+// export default function Root() {
+//   return <p>My component from app1 is mounted! Hello world</p>;
+// }
+
+const RootWrapper: React.FC<any> = (props) => {
+  console.log(props);
   return <p>My component from app1 is mounted! Hello world</p>;
-}
+};
+
+export default RootWrapper;
+
