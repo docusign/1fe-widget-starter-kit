7ce86efb0cb6305a7deb860717f3e3708bbeffd6
Sushruth Sastry <2114206+sushruth@users.noreply.github.com>
2025-03-31 13:16:19 -0700

    feat: contracts installation has been implemented (#39)
    * chore: cleaning up unnecessary files
    
    * chore: fix cli package reference in app2
    
    * chore: remove starter-cli since it is obsolete now
    
    * feat: generate types for 1fe-shell to consume everywhere else
    
    * chore: remove vite and use 1fe-cli for all widgets dev and build commands
    
    * feat: [cli] check for widget entry file as well for better error message when it does not exist
    
    * fix: fixing shell test types
    
    * fix: use the correct contract file for contract rollup generation
    
    * feat: contracts install command
    
    * chore: fix props type and remove a desperate comment
    
    * fix: fixed contracts for widgets
    
    * chore: fixing a typo
    
    * chore: removing empty files

--- .1fe.config.ts
+++ .1fe.config.ts
@@ -1,8 +1,8 @@
 import { OneFeConfiguration } from '@devhub/cli';
 import { getBaseConfig } from '@repo/widget-base-config'; // this is the redistributed package for the organization
 
-const confiugration: OneFeConfiguration = {
+const configuration: OneFeConfiguration = {
   baseConfig: getBaseConfig,
 };
 
-export default confiugration;
+export default configuration;
