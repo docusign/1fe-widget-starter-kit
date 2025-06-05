9f218ab4dce5b5588ac4453e72a8c0e9dac309be
Chaitanya Varma <chaitanya.varma@docusign.com>
2025-06-05 16:20:12 -0700

    cleanup

--- src/components/customLogger/index.tsx
+++ src/components/customLogger/index.tsx
@@ -1,4 +1,6 @@
-import { Button, Flex, Card } from 'antd';
+/** @jsxImportSource @emotion/react */
+
+import { Button } from 'antd';
 import { platformProps } from '@1fe/shell';
 import { utilSectionStyles } from '../routes/styles/utilsDemo.styles';
 
@@ -7,26 +9,22 @@ import { utilSectionStyles } from '../routes/styles/utilsDemo.styles';
  */
 export const CustomLoader: React.FC = () => {
   return (
-    // <div css={utilSectionStyles} data-qa='utils.experience.container'>
-    //   <Card title="Custom Utility: Console Logger" style={{ width: '650px' }}>
-    //     <Flex gap={5}>
-    //       <Button
-    //         data-qa='utils.experience.title.set'
-    //         onClick={() => platformProps.utils.
-    //       >
-    //         utils.logger.log
-    //       </Button>
-    //       <Button
-    //         data-qa='utils.experience.title.set'
-    //         onClick={() =>
-    //           platformProps.utils.logger.error('Example error message')
-    //         }
-    //       >
-    //         utils.logger.error
-    //       </Button>
-    //     </Flex>
-    //   </Card>
-    // </div>
-    <></>
+    <div css={utilSectionStyles} data-qa='utils.experience.container'>
+      <h1>utils.custom.logger</h1>
+      <Button
+        data-qa='utils.experience.title.set'
+        onClick={() => platformProps.utils.logger.log('Example log message')}
+      >
+        utils.logger.log
+      </Button>
+      <Button
+        data-qa='utils.experience.title.set'
+        onClick={() =>
+          platformProps.utils.logger.error('Example error message')
+        }
+      >
+        utils.logger.error
+      </Button>
+    </div>
   );
 };
