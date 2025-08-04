c4569654890f912670924d67df8dff4609630541
Mohit Khatri <mohit.khatri@docusign.com>
2025-08-04 12:19:10 -0700

    Fixing e2e test

--- tests/exampleVisual.spec.ts
+++ tests/exampleVisual.spec.ts
@@ -6,7 +6,7 @@ const version = process.env.WIDGET_VERSION || "1.0.1";
 test("should display the expected string on the bathtub page", async ({
   page,
 }) => {
-  const url = `https://demo.1fe.com/bathtub?widgetUrl=https://1fe-a.akamaihd.net/integration/widgets/${packageJson.name}/${version}/js/1fe-bundle.js&fixPreview=true`;
+  const url = `https://demo.1fe.com/playground?widgetUrl=https://1fe-a.akamaihd.net/integration/widgets/${packageJson.name}/${version}/js/1fe-bundle.js&fixPreview=true`;
   await page.goto(url);
 
   await expect(page.getByText("Welcome to 1FE Starter Kit")).toBeVisible();
